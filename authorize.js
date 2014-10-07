a19ad4f4e044fd50df7204d6ef53dc09966ca442
Paweł Głasek <guasek@gmail.com>
2014-10-07 20:30:15 +0200

    Added auth

---
+++ authorize.js
@@ -0,0 +1,9 @@
+
+//var webView = document.createElement('webview');
+//webView.setAttribute('id', 'login');
+//webView.setAttribute('src', "https://google.com");
+//webView.setAttribute('style', 'width:640px; height:480px');
+//var body = document.getElementsByTagName("body")[0];
+//body.appendChild(webView);
+
+
--- messageFetcher.js
+++ messageFetcher.js
@@ -5,26 +5,24 @@ var messageFetcher = (function () {
 
     var createFetcher = function () {
 
-        var twitterStream = new Stream({
-            "consumer_key": "hxAbNDTGRKYeuas4JkmbS8rRV",
-            "consumer_secret": "I2ngmBWkPiAC7w3hEgyQRUzkoRpMG1AM6xZIV0LWCJGbZNtbvG",
-            "access_token_key": "713921370-4rwudTcq3Ed3l3X7o1jGt0Ao5fszv7WamkILrBfx",
-            "access_token_secret": "AnLm5nZAp6W0lJFfF3iFcHwtAU6eM9DUitqCqEti1LH59"
-        });
-
-
         var fetchFromTwitterAnd = function (displayMessage) {
             var extractMessageContent = function(message) {
                 console.log(message);
                 var tweet = messages.createTweet(message);
                 displayMessage(tweet);
             }
-            twitterStream.stream();
-            twitterStream.on('data', extractMessageContent);
 
             OAuth.initialize('5RuSK2Ve-p0purYBZuaAcNqyusk');
             OAuth.chrome_app('twitter', {}, function(oAuthToken, oAuthTokenSecret) {
+                var twitterStream = new Stream({
+                    "consumer_key": "hxAbNDTGRKYeuas4JkmbS8rRV",
+                    "consumer_secret": "",
+                    "access_token_key": oAuthToken,
+                    "access_token_secret": oAuthTokenSecret
+                });
 
+                twitterStream.stream();
+                twitterStream.on('data', extractMessageContent);
             });
         }
 
